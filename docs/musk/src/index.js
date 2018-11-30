63175515558b4318e33dce6e46f139cf526f37b6
matthiasmeierkoch <matthiasmeierkoch@hotmail.com>
2018-11-30 12:54:01 +0100

    add: html, scss and js

---
+++ docs/musk/src/index.js
@@ -0,0 +1,13 @@
+// import `.scss` files
+import "./styles.scss";
+
+// import
+// import Test from './js/test'
+// let test = new Test("Hello World!")
+// test.log()
+
+// Burgermenu Starts Here
+$('.hamburger').click (function(){
+    $(this).toggleClass('open');
+});
+
