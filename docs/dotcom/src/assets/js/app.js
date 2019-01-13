8d5f1f2d9e8cc2cc97ac2aec36f0c015d3bf9449
Lars Mäder <mail@larsmaeder.ch>
2019-01-13 15:44:11 +0100

    ADD: background classes; jquery selector

--- docs/dotcom/src/assets/js/app.js
+++ docs/dotcom/src/assets/js/app.js
@@ -1,9 +1,6 @@
 import $ from 'jquery';
 import 'what-input';
 
-// Import toggle function for jQuery library
-import './lib/jquery.func_toggle';
-
 // Foundation JS relies on a global varaible. In ES6, all imports are hoisted
 // to the top of the file so if we used`import` to import Foundation,
 // it would execute earlier than we have assigned the global variable.
@@ -16,44 +13,88 @@ require('foundation-sites');
 // the line below
 //import './lib/foundation-explicit-pieces';
 
+// Import toggle function for jQuery library
+import './lib/jquery.func_toggle';
 
 $(document).foundation();
 
 // menu switch in, passiv & out
-let navmenu = document.getElementById('nav-menu');
-let navwrapper = document.getElementById('nav-wrapper');
-let navpicture = document.getElementById('nav-picture');
-let scrollinfo = document.getElementById('scroll-info');
-$(navmenu).funcToggle('click', function() {
-    $(navwrapper).css({
-        transform: 'translateX(0)',
-    });
-    $(navpicture).css({
-        transform: 'translateX(0)'
-    });
+$('#nav-menu, #nav-menu-mobile').funcToggle('click', function() {
+    $('#nav-wrapper').css({transform: 'translateX(0)',});
+    $('#nav-picture').css({transform: 'translateX(0)'});
     $('#nav-menu-outer > p:first').html("CLOSE");
-    $(navmenu).css('transform','rotate(225deg)').css('left','65px');
+    $('#nav-menu-outer-mobile > p:first').html("CLOSE");
+    $('#nav-menu').css('transform','rotate(225deg)').css('left','65px');
+    $('#nav-menu-mobile').css('transform','rotate(225deg)');
     $('body').css('overflow-y', 'hidden');
     $('#nav-menu-outer').css('left','65px');
-    $(scrollinfo).addClass('arrowpos');
+    $('#scroll-info').addClass('arrowpos');
 }, 
 function() {
-    $(navwrapper).css({
-        transform: ''
-    });
-    $(navpicture).css({
-        transform: ''
-    });
+    $('#nav-wrapper').css({transform: ''});
+    $('#nav-picture').css({transform: ''});
     $('#nav-menu-outer > p:first').html("MENU");
-    $(navmenu).css('transform','').css('left','');
+    $('#nav-menu-outer-mobile > p:first').html("MENU");
+    $('#nav-menu').css('transform','').css('left','');
+    $('#nav-menu-mobile').css('transform','');
     $('body').css('overflow-y', '');
     $('#nav-menu-outer').css('left','');
-    $(scrollinfo).removeClass('arrowpos');
+    $('#scroll-info').removeClass('arrowpos');
 });
 
 // background picture change function
-$(".evan").hover(function(){
-    $(navpicture).css("background-image", "url('../preview/preview.jpg')");
+$(".anonymous").hover(function(){
+    $('#nav-picture').css("background-image", "url('../anonymous/preview.jpg')");
     }, function(){
-    $(navpicture).css("background-image", "url('../dotcom/preview.jpg')");
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".fake").hover(function(){
+    $('#nav-picture').css("background-image", "url('../fake/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".manning").hover(function(){
+    $('#nav-picture').css("background-image", "url('../manning/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".snowden").hover(function(){
+    $('#nav-picture').css("background-image", "url('../snowden/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".musk").hover(function(){
+    $('#nav-picture').css("background-image", "url('../musk/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".spiegel").hover(function(){
+    $('#nav-picture').css("background-image", "url('../spiegel/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".levy").hover(function(){
+    $('#nav-picture').css("background-image", "url('../levy/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".verou").hover(function(){
+    $('#nav-picture').css("background-image", "url('../verou/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".mayer").hover(function(){
+    $('#nav-picture').css("background-image", "url('../mayer/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".baker").hover(function(){
+    $('#nav-picture').css("background-image", "url('../baker/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
+});
+$(".wong").hover(function(){
+    $('#nav-picture').css("background-image", "url('../wong/preview.jpg')");
+    }, function(){
+    $('#nav-picture').css("background-image", "url('../dotcom/preview.jpg')");
 });
