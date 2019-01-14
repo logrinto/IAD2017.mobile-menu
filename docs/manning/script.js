90b532b2be21092639e30fcea4b7c01ff9890782
Natasha Ruf <natasha.ruf@gmail.com>
2019-01-14 12:43:02 +0100

    text hinzufügen und anpassen

--- docs/manning/script.js
+++ docs/manning/script.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var menu = document.querySelector('.navigation--toggle');
-  var body = document.querySelector('body');
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myBtn").onclick = function() {myFunction()};
 
-  menu.addEventListener('click', function(){
-      body.classList.toggle('nav-open');
-  })
-});
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
