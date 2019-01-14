8c1e28de81510227327976829defb3d41a9b58f7
Natasha Ruf <natasha.ruf@gmail.com>
2019-01-14 19:17:36 +0100

    text korrigieren

--- docs/manning/script.js
+++ docs/manning/script.js
@@ -3,5 +3,5 @@ document.getElementById("myBtn").onclick = function() {myFunction()};
 
 /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myNavigation").classList.toggle("show");
 }
